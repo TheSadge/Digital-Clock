e6810bbb9407e539586aa16eea8c9183dbdabfa9
Sajjad Hakemzade <149905781+TheSadge@users.noreply.github.com>
2024-04-17 01:47:22 +0330

    main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function time() {
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  let currentTime = hour + ":" + minutes + ":" + seconds;
+  let currentTime = hour + ":" + minutes + ":" + seconds + session;
   let clockElement = document.querySelector("#clock");
   clockElement.innerText = currentTime;
   clockElement.textContent = currentTime;
